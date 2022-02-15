b583b50d3faf62e450e77155eac09d7da5d6fced
Alberto Lerda <albertolerda97@gmail.com>
2022-02-15 13:36:52 +0100

    Added logging

--- faucet.py
+++ faucet.py
@@ -1,31 +1,43 @@
-from flask import Flask, request
-from web3 import Web3, HTTPProvider
-from decimal import Decimal
+import datetime
 import json
+import logging
+from decimal import Decimal
+# Load env variables
 from os import environ
+
 from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+from web3 import HTTPProvider, Web3
 
 load_dotenv()
 
 SK = environ.get('SK')
 CONTRACT_ADDRESS = environ.get('CONTRACT_ADDRESS')
 FAUCET_ABI = json.loads(environ.get('FAUCET_ABI'))
-CHAIN_ID=int(environ.get('CHAIN_ID'))
-GAS_LIMIT=int(environ.get('GAS_LIMIT'))
-GWEI_PRICE=environ.get('GWEI_PRICE')
+CHAIN_ID = int(environ.get('CHAIN_ID'))
+GAS_LIMIT = int(environ.get('GAS_LIMIT'))
+GWEI_PRICE = environ.get('GWEI_PRICE')
 
 w3 = Web3(HTTPProvider('http://127.0.0.1:8545'))
 account = w3.eth.account.privateKeyToAccount(SK)
 
 oneEth = Decimal('1000000000000000000')
 
+# Setup logging
+logging.basicConfig(filename='faucet.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s|%(levelname)s|%(message)s')
+addresses_log = open('addresses.log', 'a')
+
+# Real web app
 app = Flask(__name__)
+
 
 @app.route("/")
 def request_eth():
-    balance=Decimal(w3.eth.getBalance(CONTRACT_ADDRESS))
+    balance = Decimal(w3.eth.getBalance(CONTRACT_ADDRESS))
     return f"""
-Viene utilizzato il contratto all'indirizzo f{CONTRACT_ADDRESS}<br/>
+Viene utilizzato il contratto all'indirizzo {CONTRACT_ADDRESS}<br/>
 Il bilancio attuale del faucet è {balance/oneEth} eth<br/>
 <form method="GET" action="/faucet">
     <input type="text" name="address">
@@ -41,6 +53,11 @@ def faucet_send():
     if address[0:2] != "0x":
         address = '0x' + address
 
+    if not Web3.isAddress(address):
+        logging.error("{} not an address".format(address))
+        return jsonify({'success': False,
+                        'error': "{} not an address".format(address)})
+
     faucetContract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=FAUCET_ABI)
     nonce = w3.eth.getTransactionCount(account.address)
     transfer_tx = faucetContract.functions.transfer(address).buildTransaction({
@@ -51,5 +68,8 @@ def faucet_send():
         'nonce': nonce,
     })
     signed = account.signTransaction(transfer_tx)
-    txid=w3.eth.sendRawTransaction(signed.rawTransaction).hex()
-    return f"{txid}"
+    txid = w3.eth.sendRawTransaction(signed.rawTransaction).hex()
+    addresses_log.write("{},{},{},{}\n".format(datetime.datetime.now(),
+                                               request.remote_addr,
+                                               address, txid))
+    return jsonify({'success': True, 'txid': f'{txid}'})
